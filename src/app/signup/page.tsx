2217aabe9cac48810ff492c178dd72b2a9bd4800
Neurexx <amd.riaz001@gmail.com>
2025-07-25 18:34:32 +0530

    jadfk

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -60,7 +60,7 @@ export default function SignupPage() {
             <input
               type="email"
               required
-              className="w-full px-3 py-2 border text-zinc-900 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-3 py-2 border text-zinc-900 border-gray-300 rounded-md  focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Email address"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
